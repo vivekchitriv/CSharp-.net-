a4453c8c0c2223466d7ddc9202981472e241653a
vivekchitriv <vivekchitriv786@gmail.com>
2020-09-03 13:24:46 +0530

    classwork

---
+++ classwork/Day01/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Console.WriteLine("welocome to first C#.net Project");y
+            //Console.ReadLine();
+            int i = 100;
+            int j = 200;
+
+            int result = i + j;
+            Console.WriteLine("Result is " + result.ToString());
+            Console.ReadLine();
+
+        }
+    }
+}
