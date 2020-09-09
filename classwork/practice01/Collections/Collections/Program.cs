1dc82402327357cccb8cfc5f3100ea7786073a89
vivekchitriv <vivekchitriv786@gmail.com>
2020-09-09 08:41:58 +0530

    Assignemnt 3 : WriteDetails() and ReadDetails()

---
+++ classwork/practice01/Collections/Collections/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var helicopter = new List<string>();
+            helicopter.Add("chinuke");
+            helicopter.Add("rafale");
+            helicopter.Add("AK1N");
+            helicopter.Add("Rajedd");
+
+            foreach(var hel in helicopter)
+                Console.WriteLine(hel+" ");
+            Console.ReadLine();
+        }
+    }
+}
